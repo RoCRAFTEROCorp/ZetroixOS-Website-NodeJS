170c344636435e833097821dc55ed49e0bd07a0e
Alain (ELCRAFTEROSITO) <alainbeired@gmail.com>
2025-11-19 22:02:33 +0100

    First Commit
    The ZetroixOS website files (in dev)

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3001;
+
+
+app.use('/css', express.static(path.join(__dirname, 'css')));
+
+
+app.use('/js', express.static(path.join(__dirname, 'js')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// TODO: Add New Pages to the ZetroixOS website (Soon)
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+});
